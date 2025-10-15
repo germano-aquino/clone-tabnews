4c0db628c1144606182be127a21c67873175b131
Germano Aquino <germano.r.aquino@gmail.com>
2025-10-15 14:35:43 +0000

    add: page to leila's reaction

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <>
+            <h1>Leila obg por ser essa pessoa maravilhosa!!!</h1>
+            <p>Você é incrível, sempre me dá forças eu te amo mto!!! <strong>Obg por ser minha melhor amiga!!!!</strong></p>
+        </>
+    )
+}
+
+export default Home
