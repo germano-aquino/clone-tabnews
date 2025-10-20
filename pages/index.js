2067a76c1a1dbce8338830c900a1d3ebe4b70bc4
Germano Aquino <germano.r.aquino@gmail.com>
2025-10-20 18:00:16 +0000

    add: scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 function Home() {
-    return (
-        <>
-            <h1>Leila obg por ser essa pessoa maravilhosa!!!</h1>
-            <p>Você é incrível, sempre me dá forças eu te amo mto!!! <strong>Obg por ser minha melhor amiga!!!!</strong></p>
-        </>
-    )
+  return (
+    <>
+      <h1>Leila obg por ser essa pessoa maravilhosa!!!</h1>
+      <p>
+        Você é incrível, sempre me dá forças eu te amo mto!!!{" "}
+        <strong>Obg por ser minha melhor amiga!!!!</strong>
+      </p>
+    </>
+  );
 }
 
-export default Home
+export default Home;
