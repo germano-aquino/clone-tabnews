c389bbbe79bad5cb0b120f82ceaa3e0c39ff2e08
Germano Aquino <germano.r.aquino@gmail.com>
2025-10-27 16:35:42 +0000

    finish `/status` endpoint

--- tests/integration/v1/status/get.test.js
+++ tests/integration/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const updatedAtParsed = new Date(responseBody.updated_at).toISOString();
+  expect(updatedAtParsed).toBe(responseBody.updated_at);
+
+  const databaseVersion = responseBody.dependencies.database.version;
+  expect(databaseVersion).toBe("16.10");
+
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toBe(100);
+
+  const openedConnections =
+    responseBody.dependencies.database.opened_connections;
+  expect(openedConnections).toBe(1);
 });
