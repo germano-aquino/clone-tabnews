b51e8cee09e38f65abbb6cd1dfa881e0215e06f3
germano aquino <germano.r.aquino@gmail.com>
2025-11-17 18:04:57 -0300

    feat: create `user.findByOneUsername` method and `api/v1/users/[username]` endpoint

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { ValidationError, NotFoundError } from "infra/errors";
 
 export async function create(userInputValues) {
   await validateUniqueFields(userInputValues.email, userInputValues.username);
@@ -51,8 +51,39 @@ export async function create(userInputValues) {
   }
 }
 
+export async function findOneByUsername(username) {
+  const userFound = runSelectQuery(username);
+
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: `
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        LOWER(username) = LOWER($1)
+      LIMIT
+        1
+      ;`,
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: `O 'username' informado não foi encontrado no sistema.`,
+        action: `Verifique se o 'username' está digitado corretamente.`,
+      });
+    }
+    return results.rows[0];
+  }
+}
+
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
