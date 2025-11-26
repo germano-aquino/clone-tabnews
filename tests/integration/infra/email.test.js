e2c521c79a8f8e887e8cb94f6cf0d92c49f71bf8
germano aquino <germano.r.aquino@gmail.com>
2025-11-26 20:58:10 -0300

    feat: add `email.js` infra module

---
+++ tests/integration/infra/email.test.js
@@ -0,0 +1,34 @@
+import email from "infra/email.js";
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("infra/email.js", () => {
+  test("send()", async () => {
+    await orchestrator.deleteAllEmails();
+
+    await email.send({
+      from: "Clone-Tabnew <contato@clone-tabnews.com.br>",
+      to: "contato@curso.dev",
+      subject: "Teste de assunto",
+      text: "Teste de corpo.",
+    });
+
+    await email.send({
+      from: "Clone-Tabnew <contato@clone-tabnews.com.br>",
+      to: "contato@curso.dev",
+      subject: "Ultimo email enviado",
+      text: "Corpo do ultimo email.",
+    });
+
+    const lastEmail = await orchestrator.getLastEmail();
+
+    expect(lastEmail.sender).toBe("<contato@clone-tabnews.com.br>");
+    expect(lastEmail.recipients[0]).toBe("<contato@curso.dev>");
+    expect(lastEmail.subject).toBe("Ultimo email enviado");
+    expect(lastEmail.text).toBe("Corpo do ultimo email.\n");
+    expect(Date.parse(lastEmail.created_at)).not.toBeNaN();
+  });
+});
