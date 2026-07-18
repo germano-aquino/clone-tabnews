3a1fb57048e9b465199d68e0caf44cd7f544ab0f
germano aquino <germano.r.aquino@gmail.com>
2026-07-18 19:01:06 -0300

    refactor: update home page content

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 function Home() {
   return (
     <>
-      <h1>Leila obg por ser essa pessoa maravilhosa!!!</h1>
-      <p>
-        Você é incrível, sempre me dá forças eu te amo mto!!!{" "}
-        <strong>Obg por ser minha melhor amiga!!!!</strong>
-      </p>
+      <h1>Clone Tab News</h1>
     </>
   );
 }
